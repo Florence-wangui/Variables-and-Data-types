1c789f41ee246f9114c43866f3be8ec0bb77a0bd
Florence-nyokabi <florencenyokabiwangui@gmail.com>
2023-02-02 21:40:36 +0300

    First Kotlin code!

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var fullName: String = "Florence Nyokabi"
+    println(fullName)
+
+    var age: Byte = 19
+    println(age)
+
+    var phoneNumber: Int = 792797189
+    println(phoneNumber)
+
+    var weight: Float = 43.2F
+    println(weight)
+
+    var citizen: Boolean = true
+    println(citizen)
+}
